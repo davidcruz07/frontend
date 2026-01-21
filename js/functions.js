e3179c60b841830b7fc697e1d5091591f4135ec6
Jose David Cruz Mendez <142286464+davidcruz07@users.noreply.github.com>
2026-01-20 21:20:53 -0700

    refactor: se eliminó un bloque de código que no se utilizaba

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,3 @@
-fetch('header.html').then(response => {
-    if (!response.ok) {
-      throw new Error('Error al cargar el header');
-    }
-    return response.text();
-  }).then(data => {
-    document.getElementById('header').innerHTML = data;
-
-  }).catch(error => {
-    console.error(error);
-  });
-
-
 
 const eventos = [
     {
